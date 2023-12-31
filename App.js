b56e44ad71dc740fb6c3e2cd5b04fd9f738bcd03
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2023-12-31 12:05:02 -0300

    Botão calcular novamente funcionando, falta converter valores

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity,Modal } from 'react-native';
+import {
+  StyleSheet, Text, View, TextInput,
+  Image, TouchableOpacity, Modal
+} from 'react-native';
 import ModalContent from './src/Modal';
+
+
 export default function App() {
-  const [gasolina, setGasolina] = useState();
-  const [alcool, setAlcool] = useState();
+  const [gasolina, setGasolina] = useState(0);
+  const [alcool, setAlcool] = useState(0);
+  const [visibleModal, setvisibleModal] = useState(false)
+  const [resultado, setResultado] = useState();
+
+
+  function voltar (){
+    return setvisibleModal(false)
+  }
+
+  function calcular() {
+    // Convertendo os valores para números
+    const alcoolValue = parseFloat(alcool);
+    const gasolinaValue = parseFloat(gasolina);
+  
+    if (alcoolValue === 0 || alcoolValue === "") {
+      return;
+    }
+
+    let resultado = alcoolValue / gasolinaValue
+
+    setResultado(resultado)
+
+
+    setvisibleModal(true)
+  }
 
   return (
     <View style={styles.container}>
@@ -12,29 +41,38 @@ export default function App() {
       <Text style={styles.textTitleApp}>Qual a melhor Opção ?</Text>
       <Text style={styles.textTitle}>Álcool (Preço por litro)</Text>
       <TextInput
-        keyboardType='numeric'
-        style={styles.textInput}
-        value={gasolina}
-        onChange={(value) => setGasolina(value)}
-      />
-      <Text style={styles.textTitle} >Gasolina (Preço por litro)</Text>
-      <TextInput
+      placeholder='Ex: 3,64'
         keyboardType='numeric'
         style={styles.textInput}
         value={alcool}
-        onChange={(value) => setAlcool(value)}
+        onChangeText={(value) => setAlcool(value)}
+      />
+      <Text style={styles.textTitle} >Gasolina (Preço por litro)</Text>
+      <TextInput
+        placeholder='Ex: 5,61'
+        keyboardType='numeric'
+        style={styles.textInput}
+        value={gasolina}
+        onChangeText={(value) => setGasolina(value)}
       />
 
-      <TouchableOpacity style={styles.calularBtn}>
-      <Text style={{ justifyContent: 'center', 
-      textAlign: 'center', fontSize: 25, color: '#fff', 
-      fontWeight: 'bold', height: 45,
-      borderRadius: 50,} }> Calcular </Text>
+      <TouchableOpacity style={styles.calularBtn} onPress={calcular}>
+        <Text style={{
+          justifyContent: 'center',
+          textAlign: 'center', fontSize: 25, color: '#fff',
+          fontWeight: 'bold', height: 45,
+          borderRadius: 50,
+        }}> Calcular </Text>
       </TouchableOpacity>
 
-    <Modal>
-    <ModalContent />
-    </Modal>
+
+
+      <Modal transparent={true} animationType='slide' visible={visibleModal}>
+        <ModalContent alcoolValue={alcool} 
+        gasolinaValue={gasolina} resultado={resultado} 
+          voltar={() => voltar}
+        />
+      </Modal>
 
 
       <StatusBar style="auto" />
@@ -61,13 +99,13 @@ const styles = StyleSheet.create({
     color: '#fff'
   },
   textInput: {
+    padding: 10,
     marginTop: 20,
     height: 40,
     backgroundColor: '#fff',
     width: '80%'
   },
-  calularBtn:{
-    alignContent: 'center',
+  calularBtn: {
     alignItems: 'center',
     justifyContent: 'center',
     width: '80%',
--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,27 +1,71 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-export default function ModalContent() {
+export default function ModalContent(props) {
   return (
     <View style={styles.modalcontent}>
-      <Image style={{ width: 150, height: 150, marginBottom: 20 }}
-        source={require('../../assets/gas.png')}
-      />
 
-      <Text style={styles.textResultado}> Compensa usar alcool</Text>
+      <View style={{justifyContent: 'flex-end', alignItems: 'flex-end', width: '60%'}}>
+        <TouchableOpacity style={styles.btnFechar} onPress={props.voltar}>
+          <Text style={{
+            justifyContent: 'flex-end',
+            textAlign: 'center', fontSize: 18, color: '#fff', height: 25,
+            borderRadius: 100,
+          }}>X</Text>
+        </TouchableOpacity>
+
+      </View>
+        <Image style={{ width: 150, height: 150, marginBottom: 20 }}
+          source={require('../../assets/gas.png')}
+        />
+
+      <Text style={styles.textResultado}>{props.resultado < 0.7 ? "Recomendado Abastercer Álcool " : "Recomendado Abastecer Gasolina"}</Text>
+      <View style={{ marginTop: 35 }}>
+        <Text style={styles.textInfo}> Com base nos preços:</Text>
+        <Text style={styles.textInfo}> Álcool: {props.alcoolValue}</Text>
+        <Text style={styles.textInfo}> Gasolina: {props.gasolinaValue}</Text>
+
+      </View>
+      <TouchableOpacity style={styles.calcularAgainBtn} onPress={props.voltar()}>
+        <Text style={{
+          justifyContent: 'center',
+          textAlign: 'center', fontSize: 18, color: '#fff', height: 45,
+          borderRadius: 50,
+        }}> Calcular novamente </Text>
+      </TouchableOpacity>
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   modalcontent: {
+    width:'100%',
     flex: 1,
     backgroundColor: '#121212',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   textResultado: {
     fontWeight: 'bold',
     fontSize: 25,
     color: '#13d03b'
+  },
+  textInfo: {
+    fontSize: 18,
+    marginTop: 8,
+    color: '#fff'
+  },
+  calcularAgainBtn: {
+    width: '50%',
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  btnFechar: {
+    backgroundColor: 'red',
+    width: 30,
+    borderRadius: 20,
   }
 })
