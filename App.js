48093d122ce784388b805992c944bc4eb3dff94d
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2024-01-01 22:13:36 -0300

    Adicionado animações no title e width da imagem.

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
+import React, { useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import {
-  StyleSheet, Text, View, TextInput,
-  Image, TouchableOpacity, Modal
+  StyleSheet, Text, View, TextInput, Image, TouchableOpacity, Modal, Animated
 } from 'react-native';
 import ModalContent from './src/Modal';
-
 
 export default function App() {
   const [gasolina, setGasolina] = useState(0);
   const [alcool, setAlcool] = useState(0);
-  const [visibleModal, setvisibleModal] = useState(false)
+  const [visibleModal, setvisibleModal] = useState(false);
   const [resultado, setResultado] = useState();
+  const [finishAnimate, setfinishAnimate] = useState(false);
+
+  const WidhAnimate = useRef(new Animated.Value(0)).current;
+  const heightAnimate = useRef(new Animated.Value(10)).current;
+  const textOpaciti = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+
+    Animated.sequence([
+
+      Animated.timing(WidhAnimate, {
+        toValue: 100,
+        duration: 2000,
+        useNativeDriver: false
+      }),
+      Animated.timing(heightAnimate, {
+        toValue: 150,
+        duration: 1000,
+        useNativeDriver: false
+      }),
+      Animated.timing(textOpaciti, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: false
+      })
+
+    ]).start(() => {
+      alert("Seja bem vindo ao nosso App." + "\n" + "\n" +
+        "Veja qual melhor combustivel para sua viagem")
+    });
+
+  }, [])
+
+  let porcentLargura = WidhAnimate.interpolate({
+    inputRange: [0, 100], // Entrada
+    outputRange: ['0%', '100%'] // vai sair de 0% até 100%
+  });
 
 
-  function voltar (){
+  function voltar() {
     setGasolina(0)
     setAlcool(0)
     return setvisibleModal(false)
@@ -24,32 +60,44 @@ export default function App() {
     // Convertendo os valores para números
     const alcoolValue = parseFloat(alcool);
     const gasolinaValue = parseFloat(gasolina);
-  
+
     if (alcoolValue === "" || alcoolValue === 0) {
       alert("Digite um valor valido para a Alcool")
       return;
-    }else if (gasolinaValue === "" || gasolinaValue === 0) {
+    } else if (gasolinaValue === "" || gasolinaValue === 0) {
       alert("Digite um valor valido para a Gasolina")
       return;
-
     }
 
 
     let resultado = alcoolValue / gasolinaValue
 
     setResultado(resultado)
-
-
     setvisibleModal(true)
   }
 
   return (
     <View style={styles.container}>
-      <Image style={{ width: 150, height: 150 }} source={require('./assets/logo.png')} />
-      <Text style={styles.textTitleApp}>Qual a melhor Opção ?</Text>
+
+      <Animated.View
+        style={{
+          width: porcentLargura,
+          height: heightAnimate,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'red',
+        }}
+      >
+        <Image style={{ width: 150, height: 150 }} source={require('./assets/logo.png')} />
+      </Animated.View>
+
+      <Animated.View style={{ opacity: textOpaciti }}>
+        <Text style={[styles.textTitleApp, { backgroundColor: '#121212' }]}>Qual a melhor Opção ?</Text>
+      </Animated.View>
+
       <Text style={styles.textTitle}>Álcool (Preço por litro)</Text>
       <TextInput
-      placeholder='Ex: 3,64'
+        placeholder='Ex: 3,64'
         keyboardType='numeric'
         style={styles.textInput}
         value={alcool}
@@ -73,15 +121,12 @@ export default function App() {
         }}> Calcular </Text>
       </TouchableOpacity>
 
-
-
       <Modal transparent={true} animationType='slide' visible={visibleModal}>
-        <ModalContent alcoolValue={alcool} 
-        gasolinaValue={gasolina} resultado={resultado} 
+        <ModalContent alcoolValue={alcool}
+          gasolinaValue={gasolina} resultado={resultado}
           voltar={() => voltar}
         />
       </Modal>
-
 
       <StatusBar style="auto" />
     </View>
@@ -102,7 +147,7 @@ const styles = StyleSheet.create({
     marginBottom: 50,
   },
   textTitle: {
-    marginTop: 20,
+    marginTop: 10,
     fontSize: 16,
     color: '#fff'
   },
